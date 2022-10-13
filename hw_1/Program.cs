8421ce830da3993eade099a43d7d8f517b3f6de5
Anatol Bir <you@example.com>
2022-10-13 21:51:58 +0800

    ex_5

---
+++ hw_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите значение ");
+string num = Convert.ToInt32(Console.ReadLine()).ToString();
+
+void CheckNumber(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"Ваше число: {num} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+}
+
+if (num.Length == 5)
+{
+    CheckNumber(num);
+}
+else Console.WriteLine($"Введи правильное число");
+
+
---
+++ hw_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите значение координат точек ");
+
+int SetCoordinate(string coordinateName, string pointName)
+{
+    Console.WriteLine($"{pointName}: {coordinateName} = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ax = SetCoordinate("x", "A");
+int ay = SetCoordinate("y", "A"); ;
+int az = SetCoordinate("z", "A"); ;
+int bx = SetCoordinate("x", "B"); ;
+int by = SetCoordinate("x", "B"); ;
+int bz = SetCoordinate("x", "B"); ;
+
+double result = Math.Round(Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2)), 2);
+Console.WriteLine($"result = {result}");
+
+
+
+
+
